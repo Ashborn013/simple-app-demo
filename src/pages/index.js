d4e7f71273e060739c04a090a50dfc7747fb31f3
Arjun013H <csarjun11@gmail.com>
2023-03-17 10:06:28 +0530

    fixed tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ let a = 40;
 export default function Home() {
   return (
     <>
-      <h1> Hi work in progress  bye <FaHandSparkles/> !!<br></br>{Date()}</h1>
+      <h1 className=' text-3xl'> Hi work in progress  bye <FaHandSparkles/> !!<br></br>{Date()}</h1>
      </>
   )
 }
