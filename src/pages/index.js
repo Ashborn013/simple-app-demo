bd8492fc8ccc129b4de18dc4c6b27306575ab43b
Arjun013H <csarjun11@gmail.com>
2023-03-17 09:40:15 +0530

    added date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import {FaHandSparkles} from 'react-icons/fa'
 
-
+let a = 40;
 export default function Home() {
   return (
     <>
-      <h1> Hi work in progress  bye <FaHandSparkles/> !!</h1>
+      <h1> Hi work in progress  bye <FaHandSparkles/> !!<br></br>{Date()}</h1>
      </>
   )
 }
