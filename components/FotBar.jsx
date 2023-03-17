371998cb334fe8cfb88745cd566d8d02400d52c7
Arjun013H <csarjun11@gmail.com>
2023-03-17 10:50:05 +0530

    added foter and NavBar template

---
+++ components/FotBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FotBar() {
+  return (
+    <div>FotBar</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import {FaHandSparkles} from 'react-icons/fa'
+import NavBar from 'components/NavBar';
+import FotBar from 'components/FotBar';
 
 let a = 40;
 export default function Home() {
   return (
     <>
-      <h1 className=' text-3xl'> Hi work in progress  bye <FaHandSparkles/> !!<br></br>{Date()}</h1>
+      <NavBar/>
+
+      <div className=' text-3xl flex  flex-row'> Hi work in progress  bye < FaHandSparkles/>!! </div>
+      
+      <FotBar/>
+
      </>
   )
 }
