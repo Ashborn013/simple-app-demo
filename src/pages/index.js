9af0152bb779248177d3364fcc82e538d68344a3
Arjun013H <csarjun11@gmail.com>
2023-03-28 15:48:59 +0530

    fixed the error

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <NavBar/>
 
       <div className=' text-3xl flex  flex-row'> Hi work in progress  bye < FaHandSparkles/>!! </div>
-      //<iframe src="https://discord.com/widget?id=1081962989365772288&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      {/* <iframe src="https://discord.com/widget?id=1081962989365772288&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> */}
       
       <FotBar/>
 
